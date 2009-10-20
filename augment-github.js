46e2a19e79aa0a827c324481ad62155d1631d5b8
Pat Nakajima <patnakajima@gmail.com>
2009-10-20 17:04:33 -0400

    First pass at adding issue counts

--- augment-github.js
+++ augment-github.js
@@ -1,14 +1,31 @@
 (function($) {
+  var apiURL = 'http://github.com/api/v2/json/';
+
   function appendStat(name, val) {
     return $('<span><b>' + val + '</b> ' + name + '</span>');
   };
+
+  function getIssues(user, repo) {
+    return function() {
+      var issuesURL = apiURL + 'issues/list/' + user + '/' + repo + '/open';
+      $.getJSON(issuesURL + '?callback=?', function(data) {
+        if (data.issues) {
+          $('#' + [user, repo, 'stats'].join('-')).append(appendStat('issues', data.issues.length));
+        }
+      });
+    }
+  }
 
   function augmentRepo(repo) {
     var repo = this;
 
     var elem = $('.repo_list li a[href=\"' + repo.url + '\"]');
+    
+    if (!elem.size()) { return; } // Don't augment hidden repositories.
 
-    var stats = $('<div></div>').addClass('some-repo-stats');
+    var stats = $('<div></div>')
+      .attr('id', [repo.owner, repo.name, 'stats'].join('-'))
+      .addClass('some-repo-stats');
     var watchers = appendStat('watchers', repo.watchers);
     var forks    = appendStat('forks', repo.forks);
 
@@ -29,11 +46,14 @@
 
     elem.after(stats);
     stats.slideDown();
+
+    // Load issues in the background, since each requires API hit
+    window.setTimeout(getIssues(repo.owner, repo.name), 100);
   };
 
   function fetchRepos() {
-    var apiURL = 'http://github.com/api/v2/json/';
-    var reposURL = apiURL + 'repos/show/' + $('.userbox .name').text();
+    var username = $('.userbox .name').text();
+    var reposURL = apiURL + 'repos/show/' + username;
     $.facebox($('<div>Loading repo data...</div>').css({
       textAlign: 'center'
     }));
